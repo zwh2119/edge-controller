b868c070c37738e78110ac23864e19072d7c2226
onecheck <1548767361@qq.com>
2023-11-20 22:24:29 +0800

    update AutoEdge v0.0.2

--- controller_server.py
+++ controller_server.py
@@ -1,4 +1,5 @@
 import asyncio
+import copy
 
 from fastapi import FastAPI, BackgroundTasks
 
@@ -6,6 +7,8 @@ from fastapi.routing import APIRoute
 from starlette.responses import JSONResponse
 from starlette.requests import Request
 from fastapi.middleware.cors import CORSMiddleware
+
+from utils import *
 
 
 class ControllerServer:
@@ -19,18 +22,68 @@ class ControllerServer:
                      ),
         ], log_level='trace', timeout=6000)
 
+        self.local_address = ''
+
         self.app.add_middleware(
             CORSMiddleware, allow_origins=["*"], allow_credentials=True,
             allow_methods=["*"], allow_headers=["*"],
         )
 
-    async def service_transmit(self, data: dict):
-        pass
+    def service_transmit(self, data: dict):
+        # TODO: transmit task to service
+
+        pipeline = data['pipeline_flow']
+        tmp_data = data['tmp_data']
+        index = data['cur_flow_index']
+        content = data['content_data']
+        scenario = data['scenario_data']
+
+        # record transmit time
+        tmp_data, transmit_time = record_time(tmp_data, f'transmit_time_{index}')
+        assert transmit_time != 0
+        pipeline[index]['execute_data']['transmit_time'] = transmit_time
+
+        while index < len(pipeline):
+            cur_service = pipeline[index]
+            if cur_service['execute_address'] != self.local_address:
+                tmp_data, transmit_time = record_time(tmp_data, f'transmit_time_{index}')
+                assert transmit_time == 0
+
+                data['pipeline_flow'] = pipeline
+                data['tmp_data'] = tmp_data
+                data['cur_flow_index'] = index
+                data['content_data'] = content
+                data['scenario_data'] = scenario
+
+                # TODO: post to other controllers
+
+                return
+
+            tmp_data, service_time = record_time(tmp_data, f'service_time_{index}')
+            assert service_time == 0
+
+            # TODO: post to service
+            service_return = {}
+
+            tmp_data, service_time = record_time(tmp_data, f'service_time_{index}')
+            assert service_time != 0
+            pipeline[index]['execute_data']['service_time'] = service_time
+            scenario.update(service_return['parameters'])
+            content = copy.deepcopy(service_return['result'])
+
+            index += 1
+
+        data['pipeline_flow'] = pipeline
+        data['tmp_data'] = tmp_data
+        data['cur_flow_index'] = index
+        data['content_data'] = content
+        data['scenario_data'] = scenario
+        # TODO: post to distributor
 
     async def deal_data(self, request: Request, backtask: BackgroundTasks):
         data = await request.json()
         backtask.add_task(self.service_transmit, data)
-        return {'mag': 'data send success!'}
+        return {'msg': 'data send success!'}
 
 
 app = ControllerServer().app
--- utils.py
+++ utils.py
@@ -1,6 +1,11 @@
-def decode_data():
-    pass
+import time
 
 
-def encode_data():
-    pass
+def record_time(data: dict, time_sign: str):
+    if time_sign in data:
+        start_time = data[time_sign]
+        end_time = time.time()
+        return data, end_time-start_time
+    else:
+        data[time_sign] = time.time()
+        return data, 0
