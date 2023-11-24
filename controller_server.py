20b544e8a9185cfe6c5344e3be9491f6232cea71
onecheck <1548767361@qq.com>
2023-11-25 00:03:00 +0800

    update AutoEdge v0.0.5

--- controller_server.py
+++ controller_server.py
@@ -1,8 +1,11 @@
 import asyncio
 import copy
+import json
+import os
+import shutil
 
 import requests
-from fastapi import FastAPI, BackgroundTasks
+from fastapi import FastAPI, BackgroundTasks, UploadFile, File, Form
 
 from fastapi.routing import APIRoute
 from starlette.responses import JSONResponse
@@ -10,6 +13,8 @@ from starlette.requests import Request
 from fastapi.middleware.cors import CORSMiddleware
 
 from utils import *
+
+local_port = 9002
 
 
 class ControllerServer:
@@ -23,7 +28,7 @@ class ControllerServer:
                      ),
         ], log_level='trace', timeout=6000)
 
-        self.local_address = ''
+        self.local_address = get_merge_address(get_host_ip(), port=local_port, path='submit_task')
         self.distribute_address = 'http://114.212.81.11:5713/distribute'
 
         self.app.add_middleware(
@@ -31,69 +36,85 @@ class ControllerServer:
             allow_methods=["*"], allow_headers=["*"],
         )
 
-    def service_transmit(self, data: dict):
-        # TODO: transmit task to service
+    def service_transmit(self, data, file):
 
+        data = json.loads(data)
+
+        # get json data
+        source_id = data['source_id']
+        task_id = data['task_id']
         pipeline = data['pipeline_flow']
         tmp_data = data['tmp_data']
         index = data['cur_flow_index']
-        content = data['content_data']
         scenario = data['scenario_data']
+        content = data['content_data']
 
-        print('tmp data type:', type(tmp_data))
-        print('tmp:', tmp_data)
+        # get file data(video)
+        tmp_path = f'tmp_receive_source_{source_id}_task_{task_id}.mp4'
+        with open(tmp_path, 'wb') as buffer:
+            shutil.copyfileobj(file.file, buffer)
+            del file
 
-        # record transmit time
+        # end record transmit time
         tmp_data, transmit_time = record_time(tmp_data, f'transmit_time_{index}')
         assert transmit_time != 0
         pipeline[index]['execute_data']['transmit_time'] = transmit_time
 
+        # execute pipeline
         while index < len(pipeline):
             cur_service = pipeline[index]
-            # if cur_service['execute_address'] != self.local_address:
-            #     tmp_data, transmit_time = record_time(tmp_data, f'transmit_time_{index}')
-            #     assert transmit_time == 0
-            #
-            #     data['pipeline_flow'] = pipeline
-            #     data['tmp_data'] = tmp_data
-            #     data['cur_flow_index'] = index
-            #     data['content_data'] = content
-            #     data['scenario_data'] = scenario
-            #
-            #     # TODO: post to other controllers
-            #
-            #     return
 
+            # transfer to another controller
+            if cur_service['execute_address'] != self.local_address:
+                tmp_data, transmit_time = record_time(tmp_data, f'transmit_time_{index}')
+                assert transmit_time == 0
+
+                data['pipeline_flow'] = pipeline
+                data['tmp_data'] = tmp_data
+                data['cur_flow_index'] = index
+                data['content_data'] = content
+                data['scenario_data'] = scenario
+
+                # TODO: post to other controllers
+
+                return
+
+            # start record service time
             tmp_data, service_time = record_time(tmp_data, f'service_time_{index}')
             assert service_time == 0
 
-            # TODO: post to service
-            # service_response = requests.post(cur_service['execute_address'], json={'input': content})
-            service_response = requests.post('http://127.0.0.1:9001/predict', json={'input': content})
-
+            # post to service
+            # TODO: address check
+            service_response = requests.post(cur_service['execute_address'],
+                                             data={'data': json.dumps(content)},
+                                             files={
+                                                 'file': (f'tmp_{source_id}.mp4', open(tmp_path, 'wb'), 'video/mp4')
+                                             })
             service_return = service_response.json()
 
+            # end record service time
             tmp_data, service_time = record_time(tmp_data, f'service_time_{index}')
             assert service_time != 0
             pipeline[index]['execute_data']['service_time'] = service_time
 
+            # deal with service result
             scenario.update(service_return['parameters'])
             content = copy.deepcopy(service_return['result'])
-            time.sleep(1)
 
             index += 1
 
+        # post to distributor
         data['pipeline_flow'] = pipeline
         data['tmp_data'] = tmp_data
         data['cur_flow_index'] = index
         data['content_data'] = content
         data['scenario_data'] = scenario
-        # TODO: post to distributor
         requests.post(self.distribute_address, json=data)
 
-    async def deal_response(self, request: Request, backtask: BackgroundTasks):
-        data = await request.json()
-        backtask.add_task(self.service_transmit, data)
+        os.remove(tmp_path)
+
+    async def deal_response(self, backtask: BackgroundTasks, file: UploadFile = File(...), data: str = Form(...)):
+        backtask.add_task(self.service_transmit, data, file)
         return {'msg': 'data send success!'}
 
 
--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import socket
 import time
 
 
@@ -5,7 +6,40 @@ def record_time(data: dict, time_sign: str):
     if time_sign in data:
         start_time = data[time_sign]
         end_time = time.time()
-        return data, end_time-start_time
+        return data, end_time - start_time
     else:
         data[time_sign] = time.time()
         return data, 0
+
+
+def get_merge_address(ip, protocal='http', port=None, path=None):
+    """
+    merge address from {protocal, ip, port, path}
+    eg: http://127.0.0.1:9000/submit
+    """
+
+    port_divider = '' if port is None else ':'
+    path_divider = '' if path is None else '/'
+
+    port = '' if port is None else port
+    path = '' if path is None else path
+
+    return f'{protocal}://{ip}{port_divider}{port}{path_divider}{path}'
+
+
+def get_host_ip():
+    """
+    get ip of current local host
+    """
+
+    ip = '127.0.0.1'
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(("8.8.8.8", 80))
+        ip = s.getsockname()[0]
+    except Exception:
+        pass
+    finally:
+        s.close()
+
+    return ip
